c6084b7225d0902cb5084b0e73654e8e77297536
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-05 14:58:16 +0200

    Add Console WriteLine & Write

--- P1PrintingOutput/Program.cs
+++ P1PrintingOutput/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello World!");
+// Output: Hello World!
+Console.WriteLine("Covfefe");
+// Output: Covfefe
+Console.WriteLine(5);
+// Output: 5
+Console.WriteLine("enemy");
+// Output: UnityEngine.GameObject
+Console.Write("Hello");
+Console.Write("World");
+Console.Write(5);
