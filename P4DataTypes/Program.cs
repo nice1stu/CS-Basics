9d71569ceb7aa3b446dd6f347579700750547a8b
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-05 18:05:50 +0200

    add float variable

---
+++ P4DataTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string outputString;
+outputString = "Hello World!";
+
+float decimalValue = 0.3f;
+
+Console.WriteLine(outputString);
+Console.WriteLine(decimalValue);
