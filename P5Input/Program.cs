8000c3dc0bae4ab7c19822cdf3c2cc66a691a5d3
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-05 19:03:14 +0200

    Assign input to name, print out result

---
+++ P5Input/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("What is your name?");
+Console.WriteLine("Type in your name then press [ENTER]");
+string name = Console.ReadLine();
+Console.Write("Hello ");
+Console.Write(name);
+Console.Write("!");
