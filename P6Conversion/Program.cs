7a9685499003d2313764a9194b4eff9bd0b08663
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-05 20:17:17 +0200

    Tried Debugging

--- P6Conversion/Program.cs
+++ P6Conversion/Program.cs
@@ -1,10 +1,21 @@
 ﻿
 
 Console.WriteLine("Please type in a number with decimal (.), then press [ENTER]");
-string answer = Console.ReadLine();
+string answer = Console.ReadLine(); //Get number, store in answer
+string originalAnswer = answer;
 
+Console.WriteLine(originalAnswer); // Checking originalAnswer = answer
+
+// Convert to Double Number
 double doubled = Convert.ToDouble(answer);
 Console.WriteLine(doubled);
 
+Console.WriteLine(answer); // Checking value of answer
+
+//Convert from Double to int Number
 int intNumber = Convert.ToInt32(doubled);
 Console.WriteLine(intNumber);
+
+//Convert original answer to int
+int intNumber2 = Convert.ToInt32(originalAnswer);
+Console.WriteLine(intNumber2);
