bb1aa8565a5f3241f5d10252d3a08937f5cc314f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 20:29:04 +0200

    generate 3 double 0 - 0.5

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -1,4 +1,23 @@
-﻿string seed = Console.ReadLine();
+﻿Console.WriteLine("Please pass me a Seed");
+string seed = Console.ReadLine();
 int seedInt = Convert.ToInt32(seed);
-Console.WriteLine(seed);
+
+// Generate 3 Random int
+Console.WriteLine("Three integers between 0 and 5");
+Random random = new Random(seedInt);
+int num1 = random.Next(0, 5);
+Console.WriteLine(num1);
+int num2 = random.Next(0, 5);
+Console.WriteLine(num2);
+int num3 = random.Next(0, 5);
+Console.WriteLine(num3);
+Console.WriteLine(seedInt);
+//Generate 3 random float
+Console.WriteLine("Three numbers between 0.0 and 0.5");
+double num4 = random.NextDouble()*0.5;
+Console.WriteLine(num4);
+double num5 = random.NextDouble()*0.5;
+Console.WriteLine(num5);
+double num6 = random.NextDouble()*0.5;
+Console.WriteLine(num6);
 
