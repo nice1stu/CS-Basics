b8bb924b64eeb48b46e2f12ccaa693985f62aa36
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 11:07:03 +0200

    deleted excess code

--- P9Strings/Program.cs
+++ P9Strings/Program.cs
@@ -4,6 +4,5 @@ Console.WriteLine("What's your second name");
 string secondName = Console.ReadLine();
 Console.WriteLine("What's your age");
 string age = Console.ReadLine();
-int ageInt = Convert.ToInt32(age);
-string message = $"Hello {firstName} {secondName}, you are {ageInt} years old!";
+string message = $"Hello {firstName} {secondName}, you are {age} years old!";
 Console.WriteLine(message);
