a33d8fc7bac4b1050b3cec3219fe7e403260a161
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 09:40:00 +0200

    made answer variable double

--- P7Operators/Program.cs
+++ P7Operators/Program.cs
@@ -1,4 +1,9 @@
 ﻿// Solve Add 7 to 5, multiply the result with 3, subtract 6 from that, divide that result by 1.2 and then take the remainder after division with 17 from that
-int answer;
+double answer;
 answer = ((((7 + 5) * 3) - 6) / 1.2) - 17;
+/*answer = 7 + 5;
+answer = answer * 3;
+answer = answer - 6;
+answer = answer / 1.2;
+answer = answer - 17;*/
 Console.WriteLine(answer);
