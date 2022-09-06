21947ea780916a8f6e573f57156e00f0cfbeeb39
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 20:33:28 +0200

    random 0.2-0.7

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -12,12 +12,18 @@ Console.WriteLine(num2);
 int num3 = random.Next(0, 5);
 Console.WriteLine(num3);
 Console.WriteLine(seedInt);
-//Generate 3 random float
-Console.WriteLine("Three numbers between 0.0 and 0.5");
+//Generate 3 random float 0-0.5
 double num4 = random.NextDouble()*0.5;
 Console.WriteLine(num4);
 double num5 = random.NextDouble()*0.5;
 Console.WriteLine(num5);
 double num6 = random.NextDouble()*0.5;
 Console.WriteLine(num6);
-
+//Generate 3 random float 0.2-0.7
+Console.WriteLine("Three numbers between 0.2 and 0.7");
+double num7 = random.NextDouble()*(0.7-0.2)+0.2;
+Console.WriteLine(num7);
+double num8 = random.NextDouble()*(0.7-0.2)+0.2;
+Console.WriteLine(num8);
+double num9 = random.NextDouble()*(0.7-0.2)+0.2;
+Console.WriteLine(num9);
