34210d637a49171675918baf850cf359af0dbee6
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 20:50:52 +0200

    Generate critical attacks but no output

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -12,6 +12,7 @@ Console.WriteLine(num2);
 int num3 = random.Next(0, 5);
 Console.WriteLine(num3);
 Console.WriteLine(seedInt);
+
 //Generate 3 random float 0-0.5
 double num4 = random.NextDouble()*0.5;
 Console.WriteLine(num4);
@@ -19,6 +20,7 @@ double num5 = random.NextDouble()*0.5;
 Console.WriteLine(num5);
 double num6 = random.NextDouble()*0.5;
 Console.WriteLine(num6);
+
 //Generate 3 random float 0.2-0.7
 Console.WriteLine("Three numbers between 0.2 and 0.7");
 double num7 = random.NextDouble()*(0.7-0.2)+0.2;
@@ -27,3 +29,31 @@ double num8 = random.NextDouble()*(0.7-0.2)+0.2;
 Console.WriteLine(num8);
 double num9 = random.NextDouble()*(0.7-0.2)+0.2;
 Console.WriteLine(num9);
+
+//Generate 5 random attacks to determine Crit success
+Console.WriteLine("Give me a crit chance between 0.0 (0%) and 1.0 (100%)");
+string crit = Console.ReadLine();
+double critInt = Convert.ToDouble(crit);
+
+double crit1 = random.NextDouble()*critInt+0.3;
+Console.WriteLine(crit1);
+string attack1 = crit1 > critInt ? "Critcal!" : "No Critical";
+double crit2 = random.NextDouble()*critInt+0.3;
+Console.WriteLine(crit2);
+string attack2 = crit2 > critInt ? "Critcal!" : "No Critical";
+double crit3 = random.NextDouble()*critInt+0.3;
+Console.WriteLine(crit3);
+string attack3 = crit3 > critInt ? "Critcal!" : "No Critical";
+double crit4 = random.NextDouble()*critInt+0.3;
+Console.WriteLine(crit4);
+string attack4 = crit4 > critInt ? "Critcal!" : "No Critical";
+double crit5 = random.NextDouble()*critInt+0.3;
+Console.WriteLine(crit5);
+//string attack5 = crit5 > critInt ? "Critcal!" : "No Critical";
+string attack5;
+if (crit5 < critInt) {
+    attack5 = "Critical!";
+}
+else {
+    attack5 = "No Critical";
+}
