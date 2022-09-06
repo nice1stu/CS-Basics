5a09cf24e080cc46cafe2a5afb6e1643db2eb4f7
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 09:31:20 +0200

    Add variable answer & equation to P7

---
+++ P7Operators/Program.cs
@@ -0,0 +1,4 @@
+﻿// Solve Add 7 to 5, multiply the result with 3, subtract 6 from that, divide that result by 1.2 and then take the remainder after division with 17 from that
+int answer;
+answer = ((((7 + 5) * 3) - 6) / 1.2) - 17;
+Console.WriteLine(answer);
