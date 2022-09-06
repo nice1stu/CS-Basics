b766fe6bb1365d244871e1ec3d08320c15db306f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 21:32:15 +0200

    Create instructions for Nim

---
+++ Nim/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Welcome to Nim!");
+Console.WriteLine("How to play");
+Console.WriteLine("2 Players");
+Console.WriteLine("Players take turns to draw matches");
+Console.WriteLine("Each player may draw 1,2 or 3 matches (not more or less)");
+Console.WriteLine("The Player who has to take last match loses");
+Console.WriteLine("Good Luck!");
+
+
