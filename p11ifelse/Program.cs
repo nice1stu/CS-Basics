4958c41e925814149a915cb1954cba7027fce087
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 13:50:22 +0200

    If Else print true answer

--- p11ifelse/Program.cs
+++ p11ifelse/Program.cs
@@ -1,7 +1,9 @@
-﻿Console.WriteLine("What is your age?");
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("What is your age?");
 string age = Console.ReadLine();
 int ageInt = Convert.ToInt32(age);
-
+/*
 bool isChild = ageInt < 13;
 Console.WriteLine(isChild);
 
@@ -9,13 +11,14 @@ bool isTeenager = !isChild && ageInt < 19;
 Console.WriteLine(isTeenager);
 
 bool isGrownup = ageInt > 18;
-Console.WriteLine(isGrownup);
-
-Console.Write("You are a child:");
-Console.WriteLine(isChild);
-
-Console.Write("You are a teenager:");
-Console.WriteLine(isTeenager);
-
-Console.Write("You are a grown-up:");
-Console.WriteLine(isGrownup);
+Console.WriteLine(isGrownup);*/
+      
+if (ageInt < 13) {
+    Console.WriteLine("You are a child");
+}
+else if (ageInt < 19) {
+    Console.WriteLine("You are a Teenager");
+}
+else {
+    Console.WriteLine("You are a grown-up");
+}        
