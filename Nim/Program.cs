8b217dbec806b3ad1c16e80ab9dc5be06baddd21
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 21:39:19 +0200

    add players enter name

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -6,4 +6,11 @@ Console.WriteLine("Each player may draw 1,2 or 3 matches (not more or less)");
 Console.WriteLine("The Player who has to take last match loses");
 Console.WriteLine("Good Luck!");
 
+int startMatches = 24;
+int remainingMatches;
+
+Console.WriteLine("Player 1, please enter your name");
+string player1 = Console.ReadLine();
+Console.WriteLine("Player 2, please enter your name");
+string player2 = Console.ReadLine();
 
