8b1a127c71d6a0e04591025a90f50b0669848a09
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-06 11:49:17 +0200

    Ugly code to display bool

--- P1Boolean/Program.cs
+++ P1Boolean/Program.cs
@@ -1,3 +1,25 @@
 ﻿Console.WriteLine("What is your age?");
 string age = Console.ReadLine();
 int ageInt = Convert.ToInt32(age);
+
+// isChild = >0 && <=12;
+// isTeenager = >=13 && <=19;
+// isGrownup = >19;
+
+bool isChild = ageInt < 13;
+Console.WriteLine(isChild);
+
+bool isTeenager = !isChild && ageInt < 19;
+Console.WriteLine(isTeenager);
+
+bool isGrownup = ageInt > 18;
+Console.WriteLine(isGrownup);
+
+Console.Write("You are a child:");
+Console.WriteLine(isChild);
+
+Console.Write("You are a teenager:");
+Console.WriteLine(isTeenager);
+
+Console.Write("You are a grown-up:");
+Console.WriteLine(isGrownup);
