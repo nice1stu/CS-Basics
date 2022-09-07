f04361e8564267387579515814c889a0cf3b1013
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-07 06:52:13 +0200

    introduce draw logic

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -10,6 +10,11 @@ Console.WriteLine("Good Luck!");
 //Set up game
 int startMatches = 24;
 int remainingMatches;
+int drawPlayer2i;
+int drawPlayer1i;
+string howMany = " How many matches do you want to draw?";
+
+remainingMatches = startMatches;
 
 //Players enter name
 Console.WriteLine("Player 1, please enter your name");
@@ -24,3 +29,43 @@ for (int i = 0; i < startMatches; i++)
 {
     Console.Write("|");
 }
+Console.WriteLine(remainingMatches); //Fix brackets
+
+//player1 Draw
+Console.WriteLine(player1 + howMany);
+string drawPlayer1 = Console.ReadLine();
+drawPlayer1i = Convert.ToInt32(drawPlayer1);
+
+if (drawPlayer1i > 3)
+{
+    Console.WriteLine("Please draw between 1 - 3 matches only");
+}
+// return to drawPlayer1
+remainingMatches = startMatches - drawPlayer1i;
+for (int i = 0; i < startMatches; i++)
+{
+    Console.Write("|");
+}
+Console.WriteLine(remainingMatches); //Fix brackets
+startMatches = remainingMatches;
+
+//player2 Draw
+Console.WriteLine(player2 + howMany);
+string drawPlayer2 = Console.ReadLine();
+drawPlayer2i = Convert.ToInt32(drawPlayer2);
+if (drawPlayer2i > 3)
+{
+    Console.WriteLine("Please draw between 1 - 3 matches only");
+}
+// return to drawPlayer2
+remainingMatches = startMatches - drawPlayer2i;
+for (int i = 0; i < startMatches; i++)
+{
+    Console.Write("|");
+}
+Console.WriteLine(remainingMatches); //Fix brackets
+startMatches = remainingMatches;
+
+
+Console.WriteLine(drawPlayer1i);
+Console.WriteLine(drawPlayer2i);
