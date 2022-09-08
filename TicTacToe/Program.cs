2a80bb2eac61595ee620102f196da8bb00f8dc30
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-09-08 20:47:08 +0200

    set up 1 or 2 players

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// TicTacToe
+
+Console.WriteLine("-- Welcome to Tic Tac Toe --");
+
+int numPlayers;
+string player1;
+string player2;
+
+
+//Number of players
+Console.WriteLine("Please enter the number of player 1 or 2");
+numPlayers = Convert.ToInt32(Console.ReadLine());
+numPlayers = (int)Math.Clamp((double)numPlayers, 1, 2);
+if (numPlayers == 2)
+{
+    //Players enter name
+    Console.WriteLine("Player 1, please enter your name");
+    player1 = Console.ReadLine();
+    Console.WriteLine("Player 2, please enter your name");
+    player2 = Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Player 1, please enter your name");
+    player1 = Console.ReadLine();
+    Console.WriteLine("Player 2 is HAL2000");
+    player2 = "HAL2000";
+}
